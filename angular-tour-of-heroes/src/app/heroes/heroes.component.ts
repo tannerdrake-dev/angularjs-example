5804cbe92aa5dea342d222a8a809f0138245c11e
tanner <tanner.drake@protonmail.com>
2018-01-16 10:50:14 -0500

    bind details selection to selected hero

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,15 +10,16 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-  
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
     
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
